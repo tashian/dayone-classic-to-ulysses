8a678d8fa72e66e5a2ea503807c0e0e04ea29406
tashian <carl@tashian.com>
2017-08-07 11:41:59 -0700

    Make folders for each year

--- migrate.py
+++ migrate.py
@@ -5,8 +5,8 @@ import plistlib
 import subprocess
 import re
 from datetime import timezone
-from os import listdir
-from os.path import isfile, join, splitext
+from os import listdir, mkdir
+from os.path import isfile, isdir, join, splitext
 
 ENTRIES_DIR = 'entries'
 entry_files = [f for f in listdir(ENTRIES_DIR) if
@@ -17,9 +17,18 @@ for fn in entry_files:
    with open(join(ENTRIES_DIR, fn), 'rb') as fp:
        entry = plistlib.load(fp, fmt=plistlib.FMT_XML)
 
+       # Bring the entry creation time into local timezone, which
+       # is what SetFile expects.
+       ctime = entry['Creation Date'] \
+               .replace(tzinfo=timezone.utc) \
+               .astimezone(tz=None)
+
        # Using raised colon (˸) instead of standard colon here because
        # macOS doesn't treat standard colons well in filenames
-       new_entry_fn = join('md', ctime.strftime('%B %d, %Y %H˸%M') + '.md')
+       new_entry_dir = join('md', ctime.strftime('%Y'))
+       if not isdir(new_entry_dir):
+           mkdir(new_entry_dir)
+       new_entry_fn = join(new_entry_dir, ctime.strftime('%B %d, %Y %H˸%M') + '.md')
 
        with open(new_entry_fn, 'w') as new_entry:
            # Remove all blank lines, because Ulysses treats them
@@ -32,10 +41,5 @@ for fn in entry_files:
            new_entry.write(entry_text)
 
            # Set the creation time.
-           # Bring the entry creation time into local timezone, which
-           # is what SetFile expects.
-           ctime = entry['Creation Date'] \
-                   .replace(tzinfo=timezone.utc) \
-                   .astimezone(tz=None)
            subprocess.call("SetFile -d \"{:%m/%d/%Y %H:%M:%S}\" {}".
                    format(ctime, re.sub(r' ', '\ ', new_entry_fn)), shell=True)
